d5effb987715957c8859fe16ed5f13d04fbfe04f
Purvil Patel <purvil@fintechschool.com>
2016-12-03 18:05:28 -0500

    Revert "basic lib functions defined"
    This reverts commit 40c91342d628d698612ca3bea2a388c6764e55fa.

--- lib.js
+++
@@ -1,9 +0,0 @@
-// add newline to console
-exports.newLine = function() {
-	console.log("\n");
-}
-
-// add delimiter line to console
-exports.delimiterLine = function() {
-	console.log("\n*****************************\n");
-}
