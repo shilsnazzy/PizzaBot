40c91342d628d698612ca3bea2a388c6764e55fa
Purvil Patel <purvil@fintechschool.com>
2016-12-03 17:55:05 -0500

    basic lib functions defined
    functions to add newline and delimiter line to console are defined

---
+++ lib.js
@@ -0,0 +1,9 @@
+// add newline to console
+exports.newLine = function() {
+	console.log("\n");
+}
+
+// add delimiter line to console
+exports.delimiterLine = function() {
+	console.log("\n*****************************\n");
+}
